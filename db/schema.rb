c32496112ad93d63a50aac9efd78aad61e3210ce
Erin Mitchell <erinmitchell232@gmail.com>
2018-05-23 14:39:49 +0800

    users table and festivals table in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_23_040639) do
+ActiveRecord::Schema.define(version: 2018_05_23_051156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "festivals", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.integer "price"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
