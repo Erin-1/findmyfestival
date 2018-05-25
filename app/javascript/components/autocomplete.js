1f1492d4f3d142a66019caf5e63082724183ea23
Erin Mitchell <erinmitchell232@gmail.com>
2018-05-25 12:09:15 +0800

    map with style added to index page

---
+++ app/javascript/components/autocomplete.js
@@ -0,0 +1,16 @@
+function autocomplete() {
+  document.addEventListener("DOMContentLoaded", function() {
+    var festivalAddress = document.getElementById('festival_address');
+
+    if (festivalAddress) {
+      var autocomplete = new google.maps.places.Autocomplete(festivalAddress, { types: [ 'geocode' ] });
+      google.maps.event.addDomListener(festivalAddress, 'keydown', function(e) {
+        if (e.key === "Enter") {
+          e.preventDefault(); // Do not submit the form on Enter.
+        }
+      });
+    }
+  });
+}
+
+export { autocomplete };
