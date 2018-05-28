0c285b4085344d4cda86fda6daed34c74babcc70
Erin Mitchell <erinmitchell232@gmail.com>
2018-05-28 19:02:52 +0800

    can almost add a review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,25 @@
 class ReviewsController < ApplicationController
+  def new
+    @booking = Booking.find(params[:booking_id])
+    @review = Review.new
+  end
+
+  def create
+  booking = Booking.find(params[:booking_id]
+  @review = Review.new(review_params)
+  @review.booking = booking
+  if @review.save
+  redirect_to review_path(@review)
+  else
+  render :new
+  end
+  end
+
+
+
+private
+
+def review_params
+params.require(:review).permit(:content, :stars)
+end
 end
