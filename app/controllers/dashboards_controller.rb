811a08b2211f9a37c0e542a1b8f1f90d522b947f
Erin Mitchell <erinmitchell232@gmail.com>
2018-05-29 16:44:00 +0800

    dashboard routes to my trips and my hosts created and working

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,11 +1,16 @@
 class DashboardsController < ApplicationController
 
-  def index
+  def my_festivals
     if current_user.host?
       @festivals = Festival.where(user_id: current_user)
     end
     @bookings = current_user.bookings
   end
 
+  def my_trips
+   if current_user.host?
+   @bookings = Booking.where(user_id: current_user)
+  end
+end
 
 end
